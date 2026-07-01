3f22a8a396ece911946c428f957afd3dc0d723d0
mohankumaronly <mohankumaronly81@gmail.com>
2026-07-01 16:07:44 +0530

    .env file loading in the main file

--- src/main/java/com/ranger/virtual/VirtualApplication.java
+++ src/main/java/com/ranger/virtual/VirtualApplication.java
@@ -1,5 +1,6 @@
 package com.ranger.virtual;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VirtualApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.filename(".env")
+				.ignoreIfMissing()
+				.load();
+
+		dotenv.entries().forEach(entry -> {
+			System.setProperty(entry.getKey(), entry.getValue());
+		});
+
 		SpringApplication.run(VirtualApplication.class, args);
 	}
-
 }
